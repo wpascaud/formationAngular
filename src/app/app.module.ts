a599bd0b3427a080e14429dea26a4052091f1964
PASCAUD William <william.pascaud@carsat-centreouest.fr>
2020-06-15 17:35:31 +0200

    Message de validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginModule } from './login/login.module';
+import { UiModule } from './ui/ui.module';
+import { CoreModule } from './core/core.module';
+import { IconsModule } from './icons/icons.module';
+import { TextModule } from './text/text.module';
+import { ClientsModule } from './clients/clients.module';
+import { OrdersModule } from './orders/orders.module';
+import { PageNotFoundModule } from './page-not-found/page-not-found.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -10,7 +19,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LoginModule,
+    UiModule,
+    CoreModule,
+    IconsModule,
+    TextModule,
+    ClientsModule,
+    OrdersModule,
+    PageNotFoundModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
